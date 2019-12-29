b77ef6fe06bb7d17270d6861d388182be0c23261
Somesh <someshmandhan24@gmail.com>
2019-12-29 16:53:47 +0530

    modified

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -76,7 +76,7 @@ namespace DatingApp.API.Controllers
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
-            return Ok(new {token = tokenHandler.WriteToken(token)});
+            return Ok(new {token = tokenHandler.WriteToken(token)}); 
         }
 
     }
